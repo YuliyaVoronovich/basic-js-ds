999da0bccb14b36d388e1b8454fecea926fef242
YuliyaVoronovich <Y-1306@mail.ru>
2023-12-10 13:21:45 +0300

    feat: add methods Stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor() {
+    this.items = []; 
   }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop(element) {
+    return (this.items.length === 0) ? "undefined" : this.items.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.items[this.items.length - 1]; 
   }
 }
 
